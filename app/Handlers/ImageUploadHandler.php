4f70dc45e14c29a2b7d9121f1f20d56ed148a647
vagrant <vagrant@homestead>
2020-06-14 07:16:31 +0000

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -14,7 +14,7 @@ class ImageUploadHandler
 	*@param $folader 文件上传的上级目录
 	*@param $file_prefix 文件前缀
 	*/
-	public function save($file,$folader,$file_prefix)
+	public function save($file,$folader,$file_prefix,$max_width=false)
 	{
 		//文件夹拼接规则
 		$folder_name = "uploads/images/".$folader."/".date("Ym/d",time());
@@ -31,10 +31,32 @@ class ImageUploadHandler
 		}
 		//上传文件
 		$file->move($upload_path,$filename);
+		 // 如果限制了图片宽度，就进行裁剪
+        if ($max_width && $extension != 'gif') {
 
-		return [
+            // 此类中封装的函数，用于裁剪图片
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
+        return [
             'path' => config('app.url') . "/$folder_name/$filename"
         ];
+	}
+	public function reduceSize($file_path,$max_width)
+	{
+		//
+		$image = Image::make($file_path);//实例化图片对象
+		// 进行大小调整的操作
+        $image->resize($max_width, null, function ($constraint) {
 
+            // 设定宽度是 $max_width，高度等比例缩放
+            $constraint->aspectRatio();
+
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        // 对图片修改后进行保存
+        $image->save();
 	}
 }
