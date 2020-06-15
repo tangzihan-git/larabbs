3f1f71f8f9229877d2284a8b346f2233cb93cd8e
tzh <2197486242@qq.com>
2020-06-15 01:11:53 +0000

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -18,4 +18,26 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+    //调用排序函数
+    public function scopewithOrder($query,$order)
+    {
+        switch($order){
+            case "recent":
+                $query->recent();
+            break;
+            default :
+                $query->RecentReplied();
+        }
+    }
+    //最新发布
+    public function scopeRecent($query)
+    {
+        return $query->orderBy("created_at","desc");
+
+    }
+    //最新回复
+    public function scopeRecentReplied($query)
+    {
+        return $query->orderBy("updated_at","desc");
+    }
 }
