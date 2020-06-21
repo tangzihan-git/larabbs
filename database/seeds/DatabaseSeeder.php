9f0c905fcd627bf42a1d74061ad36ba880e05520
vagrant <vagrant@homestead>
2020-06-21 12:42:41 +0000

    侧边栏链接

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
        $this->call(UsersTableSeeder::class);
         $this->call(TopicsTableSeeder::class);
         $this->call(RepliesTableSeeder::class);
-		// $this->call(TopicsTableSeeder::class);
+        $this->call(LinksTableSeeder::class);
     }
 }
 
