c0191735cbf06264b5b237185ca19ccfd3dd46b7
tzh <2197486242@qq.com>
2020-06-29 07:47:37 +0000

    活跃用户

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -60,4 +60,10 @@ class UsersController extends Controller
 
         return (new UserResource($user))->showSensitiveFields();
     }
+    //活跃用户
+    public function activedIndex(User $user)
+    {
+        UserResource::wrap('data');
+        return UserResource::collection($user->getActiveUsers());
+    }
 }
